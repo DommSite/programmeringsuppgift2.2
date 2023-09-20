46dafd90060ef06ae45c703b4c15ac8683fd4e76
DommSite <johan.johansson06@live.se>
2023-09-20 10:10:19 +0200

    Add project files.

---
+++ programmeringsuppgift2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace övning2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Alma?");
+            float almahopp = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt hoppade Elin?");
+            float elinhopp = float.Parse(Console.ReadLine());
+            float skillnad = almahopp - elinhopp;
+            Console.WriteLine("Alma hoppade " + skillnad + " meter längre än Elin");
+            Console.ReadKey();
+        }
+    }
+}
